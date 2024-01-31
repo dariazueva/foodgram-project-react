e2eed0f3eb0774237d44aee2905e8497dfd33b12
Дарья Зуева <DZueva26@gmail.com>
2024-01-31 21:59:01 +0500

    add authentification

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,6 +4,8 @@ from users.models import CustomUser
 
 
 class CustomUserSerializer(UserSerializer):
+    """Сериализатор для пользовательской модели."""
+
     class Meta:
         model = CustomUser
         fields = ('email', 'id', 'username', 'first_name', 'last_name')
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 from djoser.views import UserViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from api.serializers import CustomUserSerializer
 
 
 class CustomUserViewSet(UserViewSet):
+    """ViewSet для управления пользовательскими данными."""
 
+    permission_classes = (IsAuthenticatedOrReadOnly,)
