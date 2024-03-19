d8bb86501465c14ef2a9381899af84aa8a982764
root <root@epdfa101ds85ni239qs8.auto.internal>
2024-03-19 12:54:31 +0000

    change models users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,18 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.constants import (USER_USERNAME_MAX,
+                             USER_EMAIL_MAX,
+                             FIRST_NAME_MAX,
+                             LAST_NAME_MAX)
 
 class CustomUser(AbstractUser):
     """Модель 'Пользователь'."""
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     username = models.CharField(
         'Уникальный юзернейм',
-        max_length=150,
+        max_length=USER_USERNAME_MAX,
         unique=True
     )
-    email = models.EmailField('Адрес электронной почты', max_length=254)
-    first_name = models.CharField('Имя', max_length=150, blank=True)
-    last_name = models.CharField('Фамилия', max_length=150, blank=True)
+    email = models.EmailField('Email', max_length=USER_EMAIL_MAX,
+                               unique=True)
+    first_name = models.CharField('First name', max_length=150, blank=True)
+    last_name = models.CharField('Last name', max_length=LAST_NAME_MAX,
+                                 blank=True)
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -42,6 +50,12 @@ class Subscriptions(models.Model):
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('author', 'user'),
+                name='unique_for_author',
+            ),
+    )
 
     def __str__(self):
         return f'{self.user.username} - {self.author.username}'
